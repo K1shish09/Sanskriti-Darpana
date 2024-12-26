083ad0f555caea916f47dad2ff90265c6ca1ddc9
prerna-barde26git config --global user.name <prernabarde98@gmail.com>
2024-12-26 23:25:19 +0530

    the state info data added

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,43 @@
+// import React from 'react'
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import Home from './Pages/Home'
+// import AboutUs from './Pages/AboutUs'
+// import QuizPage from './Pages/Quiz'
+// import Ques from './Pages/Ques'
+// import Review from './Pages/Review'
+// import Login from './Pages/Login'
+// import Layout from './Layout'
+// import NewsSection from './Pages/News'
+// import Unesco from './Pages/Unesco'
+// const App = () => {
+//   return (
+//     <>
+//       <Router>
+//         <Routes>
+//           <Route element={<Layout />}>
+//             <Route path='/' element={<Home />} />
+//             <Route path='/about' element={<AboutUs />} />
+//             <Route path='/quiz' element={<QuizPage />} />
+//             <Route path='/reviews' element={<Review />} />
+//             <Route path='/news' element={<NewsSection />} />
+//             <Route path='/login' element={<Login />} />
+//             <Route path='/ques' element={<Ques />} />
+//             <Route path='/unesco' element={<Unesco />} /> 
+//           </Route>
+//         </Routes>
+//       </Router>
+//     </>
+//   )
+// }
+
+// export default App
+
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home'
-import AboutUs from './Pages/AboutUs'
-import QuizPage from './Pages/Quiz'
-import Ques from './Pages/Ques'
-import Review from './Pages/Review'
-import Login from './Pages/Login'
-import Layout from './Layout'
-import NewsSection from './Pages/News'
-import Unesco from './Pages/Unesco'
-const App = () => {
+import Intro from './Pages/Intro'
+
+function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<AboutUs />} />
-            <Route path='/quiz' element={<QuizPage />} />
-            <Route path='/reviews' element={<Review />} />
-            <Route path='/news' element={<NewsSection />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/ques' element={<Ques />} />
-            <Route path='/unesco' element={<Unesco />} /> 
-          </Route>
-        </Routes>
-      </Router>
-    </>
+    <Intro/>
   )
 }
 
