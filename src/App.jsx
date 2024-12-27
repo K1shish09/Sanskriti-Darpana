290a2c705a9b77bec34d07e5a17bef9e4bc23c48
K1shish09 <dadghayekashish@gmail.com>
2024-12-27 20:11:01 +0530

    resolved conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
-
 import AboutUs from './Pages/AboutUs'
 import QuizPage from './Pages/Quiz'
 import Ques from './Pages/Ques'
@@ -14,32 +13,23 @@ import Intro from './Pages/intro'
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    <div>
-      <Unesco/>
-    </div>
-=======
-    <>
-      <Router>
-        <Routes>
-          <Route element={<Layout />}>
-          <Route path='/' element={<Intro />} />
-            <Route path='/home' element={<Home />} />
-
-            <Route path='/about' element={<AboutUs />} />
-            <Route path='/quiz' element={<QuizPage />} />
-            <Route path='/reviews' element={<Review />} />
-            <Route path='/news' element={<NewsSection />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/ques' element={<Ques />} />
-            <Route path='/unesco' element={<Unesco />} /> 
-          </Route>
-        </Routes>
-      </Router>
-    </>
->>>>>>> 0ddae95446cae9192dbf33db2bfa96d75356d147
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/about' element={<AboutUs />} />
+          <Route path='/quiz' element={<QuizPage />} />
+          <Route path='/reviews' element={<Review />} />
+          <Route path='/news' element={<NewsSection />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/ques' element={<Ques />} />
+          <Route path='/unesco' element={<Unesco />} /> 
+          <Route path='/intro' element={<Intro />} />
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
 export default App
-
