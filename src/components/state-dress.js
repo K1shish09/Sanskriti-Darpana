13a3789ed5cc9920c7eee77552ff3bcd8286a64b
vaishnaviambarte <ambartevaishnavi@gmail.com>
2025-01-01 18:03:51 +0530

    added the data of dresses of lakshadweep and assam

--- src/components/state-dress.js
+++ src/components/state-dress.js
@@ -12,6 +12,28 @@ const stateDress = [
       ]
   },
 
+  {
+    id: "IN-LD",
+    Dresses: [
+        { name: "Mundu", description: "Traditional attire for men, consisting of a white cloth wrapped around the waist and legs, often paired with a shirt." },
+        { name: "Kachi", description: "Traditional attire for women, consisting of a long skirt and blouse, often adorned with gold jewelry." },
+        { name: "Lungi", description: "A popular garment among the locals, worn by both men and women, characterized by its vibrant colors and patterns." },
+        { name: "Thattam", description: "A headscarf worn by women, often paired with traditional attire, providing both style and modesty." },
+        { name: "Kurta-Pajama", description: "Traditional attire for men, consisting of a long tunic (kurta) and loose-fitting trousers (pajama), often worn during festivals and celebrations." }
+    ]
+ },
+ 
+ {
+    id: "IN-AS",
+    Dresses: [
+        { name: "Mekhela Chador", description: "Traditional attire for women, consisting of a two-piece garment with a skirt (mekhela) and a draped cloth (chador), often adorned with intricate designs." },
+        { name: "Dhoti-Gamosa", description: "Traditional attire for men, consisting of a dhoti (a long piece of cloth wrapped around the waist and legs) and a gamosa (a traditional towel), often worn during festivals and celebrations." },
+        { name: "Riha-Muga", description: "A traditional silk garment worn by women, often during special occasions, characterized by its elegant draping and intricate patterns." },
+        { name: "Eri Shawl", description: "A traditional shawl made from Eri silk, often worn by both men and women, providing warmth and style." },
+        { name: "Assamese Jewelry", description: "Traditional jewelry made from gold and silver, often worn by women as part of their traditional attire, including necklaces, earrings, and bangles." }
+    ]
+ },
+
 
 
 
