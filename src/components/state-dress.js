9cafe1d22baf58d8d1039cb586310c4db34bd2d0
vaishnaviambarte <ambartevaishnavi@gmail.com>
2025-01-01 17:57:47 +0530

    statedresses

---
+++ src/components/state-dress.js
@@ -0,0 +1,23 @@
+stateInfo.js
+
+const stateDress = [
+   {
+      id: 'IN-AN',
+      Dresses: [
+          { name: "Nicobarese Attire", description: "Traditional attire of the Nicobarese tribe, typically consisting of a loincloth for men and a wraparound skirt for women, often adorned with beads and shells." },
+          { name: "Lungi", description: "A popular garment among the locals, worn by both men and women, characterized by its vibrant colors and patterns." },
+          { name: "Saree", description: "Traditional attire for women, often worn during festivals and special occasions, characterized by its elegant draping and intricate designs." },
+          { name: "Kurta-Pajama", description: "Traditional attire for men, consisting of a long tunic (kurta) and loose-fitting trousers (pajama), often worn during festivals and celebrations." },
+          { name: "Shell Ornaments", description: "Traditional jewelry made from shells, often worn by both men and women as part of their traditional attire." }
+      ]
+  },
+
+
+
+
+
+];
+
+export default stateDress;
+
+
