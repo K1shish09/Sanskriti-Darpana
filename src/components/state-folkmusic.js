f4b09c805be3cb34d9860a3584384b562848d4be
vaishnaviambarte <ambartevaishnavi@gmail.com>
2025-01-01 16:25:17 +0530

    added the data of folkmusic from andaman & nicobar

---
+++ src/components/state-folkmusic.js
@@ -0,0 +1,19 @@
+stateInfo.js
+
+const stateFolkmusics = [
+    {
+        id: 'IN-AN',
+        Folkmusics: [
+            { name: "Nicobarese Chanting", description: "Traditional chanting performed by the Nicobarese tribe during various ceremonies and festivals, featuring rhythmic drumming and vocal harmonies." },
+            { name: "Lungi Songs", description: "Popular songs among the locals, characterized by vibrant rhythms and traditional instruments, often performed during celebrations." },
+            { name: "Palo Melodies", description: "Melodic tunes performed by the indigenous tribes of Andaman, often depicting their daily activities and cultural stories." },
+            { name: "Saree Songs", description: "Traditional songs performed by women wearing sarees, showcasing the cultural heritage and customs of the islands through soulful melodies." },
+            { name: "Shell Music", description: "Unique music form where performers use shells as musical instruments, creating a rhythmic and melodious performance." }
+        ]
+    },
+
+ 
+ 
+];
+
+export default stateFolkmusics;
