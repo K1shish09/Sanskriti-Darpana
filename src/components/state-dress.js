c862e0bb040b4c73ccbc6099f9542a23c8981506
vaishnaviambarte <ambartevaishnavi@gmail.com>
2025-01-05 21:00:01 +0530

    added the images of three states i.e AN, LD, AS.

--- src/components/state-dress.js
+++ src/components/state-dress.js
@@ -1,39 +1,51 @@
 stateInfo.js
 
 
+import NicobareseAttire from "../Assets/Attire/Nicobarese Attire.webp";
+import Lungi from "../Assets/Attire/Lungi.jpg";
+import Saree from "../Assets/Attire/Saree.jpg";
+import ShellOrnaments from "../Assets/Attire/Shell Ornaments.webp";
+import Mundu from "../Assets/Attire/Mundu.jpg";
+import Kachi from "../Assets/Attire/Kachi.jpg";
+import Lungi2 from "../Assets/Attire/Lungi2.jpg";
+import Thattam from "../Assets/Attire/Thattam.jpg";
+import MekhelaChador from "../Assets/Attire/Mekhela Chador.jpg";
+import DhotiGamosa from "../Assets/Attire/Dhoti-Gamosa.avif";
+import RihaMuga from "../Assets/Attire/Riha-Muga.webp";
+import EriShawl from "../Assets/Attire/Eri Shawl.webp";
+import AssameseJewelry from "../Assets/Attire/Assamese Jewelry.jpg";
 
 
 const stateDress = [
    {
       id: 'IN-AN',
       Dresses: [
-          { name: "Nicobarese Attire", description: "Traditional attire of the Nicobarese tribe, typically consisting of a loincloth for men and a wraparound skirt for women, often adorned with beads and shells." },
-          { name: "Lungi", description: "A popular garment among the locals, worn by both men and women, characterized by its vibrant colors and patterns." },
-          { name: "Saree", description: "Traditional attire for women, often worn during festivals and special occasions, characterized by its elegant draping and intricate designs." },
-          { name: "Kurta-Pajama", description: "Traditional attire for men, consisting of a long tunic (kurta) and loose-fitting trousers (pajama), often worn during festivals and celebrations." },
-          { name: "Shell Ornaments", description: "Traditional jewelry made from shells, often worn by both men and women as part of their traditional attire." }
+          { layout: NicobareseAttire, name: "Nicobarese Attire", description: "Traditional attire of the Nicobarese tribe, typically consisting of a loincloth for men and a wraparound skirt for women, often adorned with beads and shells." },
+          { layour: Lungi, name: "Lungi", description: "A popular garment among the locals, worn by both men and women, characterized by its vibrant colors and patterns." },
+          { layour: Saree, name: "Saree", description: "Traditional attire for women, often worn during festivals and special occasions, characterized by its elegant draping and intricate designs." },
+          
+          { layour: ShellOrnaments, name: "Shell Ornaments", description: "Traditional jewelry made from shells, often worn by both men and women as part of their traditional attire." }
       ]
   },
 
   {
     id: "IN-LD",
     Dresses: [
-        { name: "Mundu", description: "Traditional attire for men, consisting of a white cloth wrapped around the waist and legs, often paired with a shirt." },
-        { name: "Kachi", description: "Traditional attire for women, consisting of a long skirt and blouse, often adorned with gold jewelry." },
-        { name: "Lungi", description: "A popular garment among the locals, worn by both men and women, characterized by its vibrant colors and patterns." },
-        { name: "Thattam", description: "A headscarf worn by women, often paired with traditional attire, providing both style and modesty." },
-        { name: "Kurta-Pajama", description: "Traditional attire for men, consisting of a long tunic (kurta) and loose-fitting trousers (pajama), often worn during festivals and celebrations." }
+        { layour: Mundu, name: "Mundu", description: "Traditional attire for men, consisting of a white cloth wrapped around the waist and legs, often paired with a shirt." },
+        { layour: Kachi, name: "Kachi", description: "Traditional attire for women, consisting of a long skirt and blouse, often adorned with gold jewelry." },
+        { layour: Lungi2, name: "Lungi", description: "A popular garment among the locals, worn by both men and women, characterized by its vibrant colors and patterns." },
+        { layour: Thattam, name: "Thattam", description: "A headscarf worn by women, often paired with traditional attire, providing both style and modesty." },
     ]
  },
  
  {
     id: "IN-AS",
     Dresses: [
-        { name: "Mekhela Chador", description: "Traditional attire for women, consisting of a two-piece garment with a skirt (mekhela) and a draped cloth (chador), often adorned with intricate designs." },
-        { name: "Dhoti-Gamosa", description: "Traditional attire for men, consisting of a dhoti (a long piece of cloth wrapped around the waist and legs) and a gamosa (a traditional towel), often worn during festivals and celebrations." },
-        { name: "Riha-Muga", description: "A traditional silk garment worn by women, often during special occasions, characterized by its elegant draping and intricate patterns." },
-        { name: "Eri Shawl", description: "A traditional shawl made from Eri silk, often worn by both men and women, providing warmth and style." },
-        { name: "Assamese Jewelry", description: "Traditional jewelry made from gold and silver, often worn by women as part of their traditional attire, including necklaces, earrings, and bangles." }
+        { layour: MekhelaChador, name: "Mekhela Chador", description: "Traditional attire for women, consisting of a two-piece garment with a skirt (mekhela) and a draped cloth (chador), often adorned with intricate designs." },
+        { layour: DhotiGamosa, name: "Dhoti-Gamosa", description: "Traditional attire for men, consisting of a dhoti (a long piece of cloth wrapped around the waist and legs) and a gamosa (a traditional towel), often worn during festivals and celebrations." },
+        { layour: RihaMuga, name: "Riha-Muga", description: "A traditional silk garment worn by women, often during special occasions, characterized by its elegant draping and intricate patterns." },
+        { layour: EriShawl, name: "Eri Shawl", description: "A traditional shawl made from Eri silk, often worn by both men and women, providing warmth and style." },
+        { layour: AssameseJewelry, name: "Assamese Jewelry", description: "Traditional jewelry made from gold and silver, often worn by women as part of their traditional attire, including necklaces, earrings, and bangles." }
     ]
  },
 
