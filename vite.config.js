55d69ba651d4c9bfdf6527c718734ac382e054c1
tanvi-01543 <tanvibarbate@gmail.com>
2025-03-09 10:56:12 +0530

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 export default defineConfig({
   plugins: [react()],
-  base: "/myapp/",  // Change this if your app is in a subdirectory
+  base: "/",  // Change this if your app is in a subdirectory
   build: {
     outDir: "dist",
   },
